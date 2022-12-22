2ce40260229cf739ceb44f8aa0cb8b0d3591bb7d
juyanglin <49740728+juyanglin@users.noreply.github.com>
2022-12-22 15:24:56 +0800

    Add files via upload

---
+++ Tteacher/teacher.js
@@ -0,0 +1,112 @@
+function link( )
+		{
+            alert('許瑞陽')
+        } 
+		function link1( )
+		{
+            alert('郭盈傑')
+        } 
+		function link2( )
+		{
+            alert('林美珠')
+        } 
+		function link3( )
+		{
+            alert('顏慶瑩')
+        }
+		function link4( )
+		{
+            alert('曾鈺銘')
+        } 
+		
+        function link5( )
+		{
+            alert('溫善淳')
+        }
+			
+        function link71( )
+		{
+            alert('林建安')
+        } 
+		 function link72( )
+		{
+            alert('林如洋')
+        } 
+		 function link73( )
+		{
+            alert('沈孟儒')
+        } 
+		 function link74( )
+		{
+            alert('黃詩惠')
+        } 
+		 function link76( )
+		{
+            alert('葉怡秀')
+        } 
+		 function link75( )
+		{
+            alert('張英珍')
+        } 
+		 function link11( )
+		{
+            alert('呂琬琪')
+        } 
+		 function link13( )
+		{
+            alert('王巧沛')
+        } 
+         function link21( )
+		{
+            alert('林淑妙')
+        } 
+		 function link23( )
+		{
+            alert('吳美潔')
+        } 
+		 function link31( )
+		{
+            alert('陳靜卿')
+        } 
+		 function link33( )
+		{
+            alert('簡郁雅')
+        } 
+		  function link41( )
+		{
+            alert('林士傑')
+        } 
+		 function link43( )
+		{
+            alert('葉建宏')
+        } 
+		 function link51( )
+		{
+            alert('陳舜明')
+        } 
+		 function link52( )
+		{
+            alert('涂大節')
+        } 
+		
+		 function link61( )
+		{
+            alert('張麗美')
+        } 
+		 function link62( )
+		{
+            alert('蔡旻家')
+        } 
+		 function link63( )
+		{
+            alert('葉建宏')
+        } 
+		
+        function link34( )
+		{
+            alert('張寶月,闕妤甄')
+        } 
+		 function link100( )
+		{
+            alert('何月娥,李佩瑾,')
+        } 
